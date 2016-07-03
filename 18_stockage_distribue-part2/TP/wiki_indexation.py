1418f8a4d3aad4db6a79e5cb44b0a2fc367ab152
kyoucef <youcef.kacer@gmail.com>
2016-07-03 23:18:06 +0200

    add progress to HBASE exercises

--- 18_stockage_distribue-part2/TP/wiki_indexation.py
+++ 18_stockage_distribue-part2/TP/wiki_indexation.py
@@ -25,45 +25,47 @@ NdocsPerWord = {}
 NdocsMax = 30000
 for i,(key,data) in enumerate(table_wiki.scan()):
     url = key.decode('utf-8')
-    print "currently indexing url : ",url
-    # get text from url :
+    print "currently processing url : ",url
+    ## get text from url :
     text = data['cf:body'].decode('utf-8')
-    # tokenize text :
-    words_it = re.split('\W+',text)
-    # stem words :
-    words_stemmed = [stem(word.lower()) for word in words_it if word not in words_stop and word not in '']
-    # remove redundancy :
-    words_stemmed_unique = list(set(words_stemmed))
-    # number of words in document :
+    ## tokenize text :
+    words_stemmed_unique = [stem(word.group().lower()) for word in re.finditer(r"\w+",text,re.UNICODE) if word.group().lower() not in words_stop]
+
+    ## number of words in document :
     nT = len(words_stemmed_unique)
-    # compute tf for each word of document :
+    ## compute tf for each word of document :
     batch = table_index.batch()
     for word in words_stemmed_unique:
         if word in NdocsPerWord:
             NdocsPerWord[word] += 1 
         else:
             NdocsPerWord[word] = 1
-        nt = words_stemmed.count(word)
+        nt = words_stemmed_unique.count(word)
         tf = 1.0*nt/nT
-        # print "-- ",word,":",nt,",",tf
-        batch.put(word.encode('utf-8'),{"wiki:"+url:str(tf).encode('utf-8'),"wiki:url":url.encode('utf-8')})
+        batch.put(word.encode('utf-8'),{"wiki:"+url:str(tf).encode('utf-8')})
     batch.send()
-    print "tf : ",tf
     if i+1>=NdocsMax:
         break;
 
 Ndocs = i
 print "Total number of documents : ",Ndocs
 
-batch = table_index.batch()
-for key,data in table_index.scan():
+
+## OBLIGER DE COUPER LE SCAN EN DEUX, SINON ERREUR...
+for key,data in table_index.scan(row_stop='2240'):
     word = key.decode('utf-8')
-    url = data['wiki:url'].decode('utf-8')
     print "currently indexing word : ",word
-    tf = float(data['wiki:'+url].decode('utf-8'))
     idf = np.log10(1.0*Ndocs/NdocsPerWord[word])
-    tfidf = tf*idf
-    print "tfidf : ",tfidf
-    batch.put(word.encode('utf-8'),{"wiki:"+url:str(tfidf).encode('utf-8')})
-    batch.delete(word.encode('utf-8'),{"wiki:url"})
-batch.send()
+    for url in data.keys():
+        tf = float(data[url].decode('utf-8'))
+        tfidf = tf*idf
+        table_index.put(word.encode('utf-8'),{url:str(tfidf).encode('utf-8')})
+
+for key,data in table_index.scan(row_start='2240'):
+    word = key.decode('utf-8')
+    print "currently indexing word : ",word
+    idf = np.log10(1.0*Ndocs/NdocsPerWord[word])
+    for url in data.keys():
+        tf = float(data[url].decode('utf-8'))
+        tfidf = tf*idf
+        table_index.put(word.encode('utf-8'),{url:str(tfidf).encode('utf-8')})
