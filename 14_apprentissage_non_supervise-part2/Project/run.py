e3cc39a094148755ec058d2a81184a4233ca8720
kyoucef <youcef.kacer@gmail.com>
2016-08-08 00:14:20 +0200

    first commit for unsupervised learning project

---
+++ 14_apprentissage_non_supervise-part2/Project/run.py
@@ -0,0 +1,27 @@
+import os
+import glob
+import cv2
+import matplotlib.pyplot as plt
+
+if os.path.exists('data_papers') == False:
+    os.mkdir('data_papers')
+    if os.path.exists('dataset_segmentation.rar') == False:
+        os.system('wget https://archive.ics.uci.edu/ml/machine-learning-databases/00306/dataset_segmentation.rar')
+    else:
+        os.system('cd data_papers')
+        os.system('unrar e ../dataset_segmentation.rar')
+        os.system('cd ..')
+
+list_mask = glob.glob('data_papers/*_m.*')
+for file_mask in list_mask:
+    file_image = glob.glob(file_mask[:-6]+'.*')[0]
+    image = cv2.imread(file_image)
+    mask = cv2.imread(file_mask)
+    f = plt.figure()
+    f.add_subplot(1,2,1)
+    plt.imshow(image)
+    f.add_subplot(1,2,2)
+    plt.imshow(mask)
+    plt.show()
+
+    
