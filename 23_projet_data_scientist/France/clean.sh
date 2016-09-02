6c78cdbd455de099dbbf54ae12fecf606f3a99b7
Youcef Kacer <youssef.kacer@smithsdetection.com>
2016-09-02 19:51:04 +0200

    add header in .CSV produced by clean.sh

--- 23_projet_data_scientist/France/clean.sh
+++ 23_projet_data_scientist/France/clean.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-sed 's/\,/\t/g' LANDSAT_8_134311.csv | \
+header=`head -n 1 $1`
+echo 1i"$header"
+sed '1 d' $1 | \
+sed 's/\,/\t/g' | \
 awk 'BEGIN {OFS=FS="\t"} {gsub(/\./,",",$24)}1' | \
 sort -t $'\t' -k 2n -k 3n -k 24g | \
 awk 'BEGIN {OFS=FS="\t"} {gsub(/\,/,".",$24)}1' | \
-sed 's/\t/\,/g' \
-sort -t',' -k 2,3 -u \
-> LANDSAT_8_134311_clean.csv
+sed 's/\t/\,/g' | \
+sort -t',' -k 2,3 -u > "$1"_clean.csv
+sed -i "1i${header}" "$1"_clean.csv
+
+
