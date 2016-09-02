e4b8f068963acb30d58ee76d5bb23e995cd7ae73
kyoucef <youcef.kacer@gmail.com>
2016-09-02 22:40:02 +0200

    fix name for CSV output file

--- 23_projet_data_scientist/clean.sh
+++ 23_projet_data_scientist/clean.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+file=$1
 header=`head -n 1 $1`
-echo 1i"$header"
+
 sed '1 d' $1 | \
 sed 's/\,/\t/g' | \
 awk 'BEGIN {OFS=FS="\t"} {gsub(/\./,",",$24)}1' | \
 sort -t $'\t' -k 2n -k 3n -k 24g | \
 awk 'BEGIN {OFS=FS="\t"} {gsub(/\,/,".",$24)}1' | \
 sed 's/\t/\,/g' | \
-sort -t',' -k 2,3 -u > "$1"_clean.csv
-sed -i "1i${header}" "$1"_clean.csv
+sort -t',' -k 2,3 -u > ${file%%.*}_clean.csv
+sed -i "1i${header}" ${file%%.*}_clean.csv
 
 
