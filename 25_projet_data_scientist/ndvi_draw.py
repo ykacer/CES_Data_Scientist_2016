4aa83de020ab54e95ef64f44a874d6df3c595017
Youcef Kacer <youssef.kacer@smithsdetection.com>
2016-10-19 18:56:05 +0200

    improve python file that draws ndvi histograms for various cities

--- 25_projet_data_scientist/ndvi_draw.py
+++ 25_projet_data_scientist/ndvi_draw.py
@@ -1,7 +1,5 @@
 # coding: utf-8
 
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -13,28 +11,29 @@ data = {};
 densities = [];
 
 for row_path in sys.argv[2:]:
-    for city in os.listdir(row_path):
-        if os.path.isdir(row_path+'/'+city) and 'LC8' not in city:
-            ndvi = np.load(row_path+'/'+city+'/ndvi_histo.npy').item()
-            surface = ndvi['surface']
-            density = ndvi['density']
-            curve = ndvi[month]
-            print 'city:',city
-            print 'surface:',surface
-            print 'density:',density
-            densities.append(density)
-            data[density] = [city,curve,surface]
+	for city in os.listdir(row_path):
+        	if os.path.isdir(row_path+'/'+city) and 'LC8' not in city:
+			histo_file = row_path+'/'+city+'/ndvi_histo.npy'
+			if os.path.isfile(histo_file):
+            			ndvi = np.load(row_path+'/'+city+'/ndvi_histo.npy').item()
+				if 'surface' in ndvi.keys() and 'density' in ndvi.keys():
+            				surface = ndvi['surface']
+            				density = ndvi['density']
+            				curve = ndvi[month]
+            				print 'city:',city
+	            			print 'surface:',surface
+       		     			print 'density:',density
+					print '\n'
+       		     			densities.append(density)
+            				data[density] = [city,curve,surface]
 
 sorted_densities = np.sort(densities)
 nbins = np.linspace(-1,1,curve.size)
-print sorted_densities
 for s in sorted_densities:
     curve = data[s][1]
     name = data[s][0]
     print s,name
-    plt.plot(nbins,curve,label=unicode(name)+u' ('+str(s)+u' habs/km\^2)')
+    plt.plot(nbins,curve,label=unicode(name)+u' ('+str(s)+u' habs/km²)')
 
-plt.legend(loc=2,fontsize='medium')
-plt.show()
-
-
+plt.legend(loc=2,fontsize='small')
+plt.savefig(month+'.png')
