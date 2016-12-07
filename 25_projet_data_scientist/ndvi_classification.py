b2f5eba3178c5d777601eb4105566f13498d849c
Youcef Kacer <youssef.kacer@smithsdetection.com>
2016-12-07 18:46:43 +0100

    add PCA aas preprocessing

--- 25_projet_data_scientist/ndvi_classification.py
+++ 25_projet_data_scientist/ndvi_classification.py
@@ -82,17 +82,25 @@ elif 'PMUN16' in data.columns:
 
 surface = data['SURFACE'].as_matrix()
 densite = population/surface;
+
 # reduction
 X = data[variables].as_matrix(); 
 scaler = StandardScaler()
 scaler.fit(X)
 Xsc = scaler.transform(X)
-# projection
-pca = PCA(n_components=512);
-Xpca = pca.fit_transform(Xsc);
-print("*** PCA explained variance : "+str(pca.explained_variance_ratio_.sum()))
-y = -1*np.ones(X.shape[0])
 
+# projection
+pca = PCA(n_components=600);
+Xpca = pca.fit_transform(Xsc);
+joblib.dump(pca,u'model_classification/PCA_classification.pkl')
+fpca = codecs.open(u'model_classification/PCA_classification.txt',u'w',u'utf8')
+fpca.write(u'PCA explained variance : '+str(pca.explained_variance_ratio_.sum()*100)+u'%\n')
+fpca.close()
+
+print(u'*** PCA explained variance : '+str(pca.explained_variance_ratio_.sum()))
+
+# density categorization
+y = -1*np.ones(X.shape[0])
 categorization_r = list(reversed(categorization))
 for i,n in enumerate(densite):
     if n>categorization_r[0]:
@@ -112,63 +120,65 @@ print('*** Classe Distribution :')
 for i in np.arange(nc+1):
     print("categorie "+str(i)+": "+str((y==i).sum())+" samples")
 
-ROS = RandomOverSampler(ratio=0.3)
-Xo,yo = ROS.fit_sample(X,y)
+ROS = RandomOverSampler(ratio=0.4)
+Xo,yo = ROS.fit_sample(Xpca,y)
 print('*** Classe Distribution (after oversampling):')
 for i in np.arange(nc+1):
     print("categorie "+str(i)+": "+str((yo==i).sum())+" samples")
 
 print("*** Classification bench")
 cv = StratifiedKFold(y,n_folds=5,random_state=0) # cross-validation set
+cvo = StratifiedKFold(yo,n_folds=5,random_state=0) # cross-validation set
+
 results = [];
 verbose = 5
 
 print("* Support Vector Classification")
 cl = LinearSVC(class_weight='balanced',penalty='l1',dual=False,random_state=0,verbose=False)
-param_grid = {'C':[0.0001,0.001,0.01,0.1,1.0,10.0,100.0,1000.0,10000.0]}
+param_grid = {'C':[0.00001,0.0001,0.001,0.01,0.1,1.0,10.0,100.0,1000.0,10000.0]}
 grid = grid_search.GridSearchCV(cl,param_grid,cv=cv,verbose=verbose)
-grid.fit(X,y)
+grid.fit(Xpca,y)
 #info = "percentage of support vectors : "+1.0*len(grid.best_estimator_.support_)/y.size+"%\n"
 info=''
-info = info + np.array_str(metrics.confusion_matrix(grid.best_estimator_.predict(X), y))
+info = info + np.array_str(metrics.confusion_matrix(grid.best_estimator_.predict(Xpca), y))
 results.append(['Support Vector Classification',grid.grid_scores_,grid.scorer_,grid.best_score_,grid.best_params_,grid.get_params(),grid.best_estimator_,info])
 
 print("* Random Forest Classification")
 cl = RandomForestClassifier(n_estimators=40,max_depth=15,min_samples_split=20,min_samples_leaf=20,max_features=35,random_state=0)
 param_grid = {'max_features':[15]}
 grid = grid_search.GridSearchCV(cl,param_grid,cv=cv,verbose=verbose)
-grid.fit(X,y)
-info = np.array_str(metrics.confusion_matrix(grid.best_estimator_.predict(X), y))
+grid.fit(Xpca,y)
+info = np.array_str(metrics.confusion_matrix(grid.best_estimator_.predict(Xpca), y))
 results.append(['Random Forest Classification',grid.grid_scores_,grid.scorer_,grid.best_score_,grid.best_params_,grid.get_params(),grid.best_estimator_,info])
 
 print("* Gradient Boosting Classification")
 cl = GradientBoostingClassifier(learning_rate=0.45, n_estimators=40, min_samples_split=80,min_samples_leaf=20,max_depth=32,max_features=32, subsample=0.8, random_state=0)
 param_grid = {'n_estimators':[40]}
 grid = grid_search.GridSearchCV(cl,param_grid,cv=cv,verbose=verbose)
-grid.fit(X,y)
-info = np.array_str(metrics.confusion_matrix(grid.best_estimator_.predict(X), y))
+grid.fit(Xpca,y)
+info = np.array_str(metrics.confusion_matrix(grid.best_estimator_.predict(Xpca), y))
 results.append(['Gradient Boosting Classification',grid.grid_scores_,grid.scorer_,grid.best_score_,grid.best_params_,grid.get_params(),grid.best_estimator_,info])
 
 print("* Extreme Gradient Boosting Classification")
 cl = XGBClassifier(learning_rate =0.1,n_estimators=1000,max_depth=5,min_child_weight=1,gamma=0,subsample=0.8,colsample_bytree=0.8, objective='binary:logistic',nthread=4, scale_pos_weight=1, seed=0,silent=False)
 param_grid = {'learning_rate':[0.1]}
 grid = grid_search.GridSearchCV(cl,param_grid,cv=cv,verbose=verbose)
-grid.fit(X,y)
-info = np.array_str(metrics.confusion_matrix(grid.best_estimator_.predict(X),y))
+grid.fit(Xpca,y)
+info = np.array_str(metrics.confusion_matrix(grid.best_estimator_.predict(Xpca),y))
 results.append(['Extreme Gradient Boosting Classification',grid.grid_scores_,grid.scorer_,grid.best_score_,grid.best_params_,grid.get_params(),grid.best_estimator_,info])
 
 print("* Neural Network Classifier")
 cl = MLPClassifier(activation='logistic', batch_size='auto',learning_rate='constant', power_t=0.5, max_iter=200,random_state=0,tol=0.0001,momentum=0.9,nesterovs_momentum=True,early_stopping=False,verbose=True)
-param_grid = {'hidden_layer_sizes':[(4096,)],'solver':['sgd'],'alpha':[0.001],'learning_rate_init':[0.0001,0.001]}
-grid = grid_search.GridSearchCV(cl,param_grid,cv=cv,verbose=verbose)
-grid.fit(Xsc,y)
-info = np.array_str(metrics.confusion_matrix(grid.best_estimator_.predict(X), y))
+param_grid = {'hidden_layer_sizes':[(1024,)],'solver':['sgd'],'alpha':[0.001],'learning_rate_init':[0.0001,0.001]}
+grid = grid_search.GridSearchCV(cl,param_grid,cv=cvo,verbose=verbose)
+grid.fit(Xo,yo)
+info = np.array_str(metrics.confusion_matrix(grid.best_estimator_.predict(Xo), yo))
 results.append(['Neural Network Classification',grid.grid_scores_,grid.scorer_,grid.best_score_,grid.best_params_,grid.get_params(),grid.best_estimator_,info])
 
 print("* TensorFlow Neural Network Classification")
 def make_model():
     model = Sequential()
-    model.add(Dense(output_dim=1204,input_dim=512,init='uniform',activation='relu'))
+    model.add(Dense(output_dim=1204,input_dim=n_components,init='uniform',activation='relu'))
     model.add(Dense(512, init='uniform', activation='relu'))
     model.add(Dense(nc+1, init='uniform', activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -176,9 +186,9 @@ def make_model():
 
 cl = KerasClassifier(make_model, nb_epoch=200)
 param_grid = {'batch_size':[100]}
-grid = grid_search.GridSearchCV(cl,param_grid,cv=cv,verbose=verbose)
-grid.fit(Xsc,to_categorical(y,nc+1))
-info = np.array_str(metrics.confusion_matrix(grid.best_estimator_.predict(X),to_categorical(y,nc+1)))
+grid = grid_search.GridSearchCV(cl,param_grid,cv=cvo,verbose=verbose)
+grid.fit(Xpca,to_categorical(y,nc+1))
+info = np.array_str(metrics.confusion_matrix(grid.best_estimator_.predict(Xpca),to_categorical(y,nc+1)))
 results.append(['Neural Network Classification TensorFlow',grid.grid_scores_,grid.scorer_,grid.best_score_,grid.best_params_,grid.get_params(),grid.best_estimator_,info])
 
 try:
@@ -192,8 +202,12 @@ for res in [results[-1]]:
     joblib.dump(model,u'model_classification/'+name+u'.pkl')
     f = codecs.open(u'model_classification/'+name+u'_report.txt','w','utf8')
     f.write(res[-1]+u'\n\n')
-    f.write("error cv : "+str((1-res[3])*100)+u'%\n\n')
-    f.write(metrics.classification_report(y, model.predict(X), labels=np.arange(nc+1).tolist(), target_names=target_names,digits=3))
+    f.write(u'error cv : '+str((1-res[3])*100)+u'%\n\n')
+    f.write(u'grid scores : \n')
+    for gs in res[1]:
+        f.write(u'\t'+str(gs)+u'\n')
+    f.write(u'\n')
+    f.write(metrics.classification_report(y, model.predict(Xpca), labels=np.arange(nc+1).tolist(), target_names=target_names,digits=3))
     f.close()
 
 
