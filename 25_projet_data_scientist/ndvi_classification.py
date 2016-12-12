a36b0a5a15d8c1be20de67f828077b750aaebf15
Youcef Kacer <youssef.kacer@smithsdetection.com>
2016-12-12 19:33:05 +0100

    add Keras CNN architecture

--- 25_projet_data_scientist/ndvi_classification.py
+++ 25_projet_data_scientist/ndvi_classification.py
@@ -339,11 +339,11 @@ info = info+u'mean error per class : '+str(100-mean_scores.mean())+'%\n\n'
 results.append(['Neural Network Classification TensorFlow-oversampling',grid.grid_scores_,grid.scorer_,grid.best_score_,grid.best_params_,grid.get_params(),grid.best_estimator_,info])
 
 print("* Convolutional Neural Network Regression (Keras)")
-def make_cnn(optimizer='adam'):
+def make_cnn(loss='categorical_crossentropy',optimizer='adam'):
     model = Sequential()
-    #model.add(Convolution1D(1,16,subsample_length=16,border_mode='same',input_shape=(1,X.shape[1])))
-    #model.add(Activation('relu'))
-    #model.add(MaxPooling1D(pool_length=2))
+    model.add(Convolution1D(1,16,subsample_length=16,border_mode='same',input_shape=(X.shape[1],1)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling1D(pool_length=2))
     model.add(Convolution1D(32, 3, border_mode='same',input_shape=(1,32)))
     model.add(Activation('relu'))
     model.add(Convolution1D(32, 3))
@@ -368,7 +368,7 @@ cl = KerasClassifier(make_cnn, nb_epoch=100)
 optimizers = [SGD(lr=0.001, decay=1e-3, momentum=0.9, nesterov=True)]
 param_grid = {'batch_size':[100],'optimizer':optimizers}
 grid = grid_search.GridSearchCV(cl,param_grid,cv=cv,verbose=verbose)
-grid.fit(X[:,:32],to_categorical(y,n_cl))
+grid.fit(X.reshape(X.shape + (1,)),to_categorical(y,n_cl))
 results.append(['Convolutional Neural Network Regression (Keras)',grid.grid_scores_,grid.scorer_,grid.best_score_,grid.best_params_,grid.get_params(),grid.best_estimator_,""])
 
 
