31f7c8886123bfbf1f1f8a360ee6594568627707
Youcef Kacer <youssef.kacer@smithsdetection.com>
2017-01-02 17:14:50 +0100

    test script now deals with data that don't contain ground truth densities

--- 25_projet_data_scientist/code/python/ndvi_test_classification.py
+++ 25_projet_data_scientist/code/python/ndvi_test_classification.py
@@ -39,6 +39,7 @@ data.dropna(how='any',inplace=True)
 data = data[data.SURFACE != 0]
 print(str(data.shape[0])+" cities")
 
+ground_truth_available = True
 variables = [v for v in data.columns if v.isdigit()]
 if 'PMUN13' in data.columns:
     year = u'13'
@@ -56,29 +57,31 @@ elif 'PMUN16' in data.columns:
     year = u'16'
     data = data[data.PMUN16 != 0]
     population = data['PMUN16'].as_matrix()
+else:
+    ground_truth_available = False
 
 X = data[variables].as_matrix(); 
 surface = data['SURFACE'].as_matrix()
-densite = population/surface;
-y = -1*np.ones(X.shape[0])
-
-categorization_r = list(reversed(categorization))
-for i,n in enumerate(densite):
-    if n>categorization_r[0]:
-        y[i] = 5
-    elif n>categorization_r[1]:
-        y[i] = 4
-    elif n>categorization_r[2]:
-        y[i] = 3
-    elif n>categorization_r[3]:
-        y[i] = 2
-    elif n>categorization_r[4]:
-        y[i] = 1
-    else:
-        y[i] = 0
-
-for i in np.arange(nc+1):
-    print("categorie "+str(i)+": "+str((y==i).sum())+" samples")
+if ground_truth_available:
+    densite = population/surface;
+    y = -1*np.ones(X.shape[0])
+    categorization_r = list(reversed(categorization))
+    for i,n in enumerate(densite):
+        if n>categorization_r[0]:
+            y[i] = 5
+        elif n>categorization_r[1]:
+            y[i] = 4
+        elif n>categorization_r[2]:
+            y[i] = 3
+        elif n>categorization_r[3]:
+            y[i] = 2
+        elif n>categorization_r[4]:
+            y[i] = 1
+        else:
+            y[i] = 0
+    
+    for i in np.arange(nc+1):
+        print("categorie "+str(i)+": "+str((y==i).sum())+" samples")
 
 pca = joblib.load(u'model_classification/PCA_classification.pkl')
 scaler = joblib.load(u'model_classification/Scaler_classification.pkl')
@@ -95,21 +98,28 @@ elif model[-2:] == 'h5':
     except:
         yp = np.argmax(clf.predict(X.reshape(X.shape + (1,))),axis=1)
 
+if ground_truth_available:
+	data['TRUTH'] = y
+
 data['CLASSIFICATION'] = yp
-if model[-3:] == 'pkl':
-    error = 100*(1-clf.score(X,y))
-elif model[-2:] == 'h5':
-    try:
-        error = 100*(1-clf.evaluate(X,label_binarize(y,np.arange(nc+1)))[1])
-    except:
-        error = 100*(1-clf.evaluate(X.reshape(X.shape + (1,)),label_binarize(y,np.arange(nc+1)))[1])
-data['ERROR'] = error
-print("error : "+str(error)+"%")
+
+if ground_truth_available:
+    if model[-3:] == 'pkl':
+        error = 100*(1-clf.score(X,y))
+    elif model[-2:] == 'h5':
+        try:
+            error = 100*(1-clf.evaluate(X,label_binarize(y,np.arange(nc+1)))[1])
+        except:
+            error = 100*(1-clf.evaluate(X.reshape(X.shape + (1,)),label_binarize(y,np.arange(nc+1)))[1])
+    data['ERROR'] = yp-y
+    data['PRECISION'] = error
+    print("error : "+str(error)+"%")
 
 try:
 	os.mkdir(folder+u'/test/')
 except:
 	pass
+
 print(model)
 folder_model = folder+u'/test/'+os.path.basename(model)[:str.rfind(os.path.basename(model),'.')]+'/'
 try:
