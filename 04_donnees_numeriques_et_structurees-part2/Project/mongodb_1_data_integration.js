0bc2677c1e643d445d9583b5065a2e525713f284
kyoucef <youcef.kacer@gmail.com>
2018-11-29 20:55:45 +0100

    suppress all courses due to Telecom ParisTech complaints

--- 04_donnees_numeriques_et_structurees-part2/Project/mongodb_1_data_integration.js
+++
@@ -1,5 +0,0 @@
-db = db.getSiblingDB('enernoc')
-var n = site_id.toString();
-db.getCollection(n).remove({anomaly :{$ne:""}});
-db.getCollection(n).find().forEach(function(cs){ cs.iso_date = new Date(cs.timestamp*1000); db.getCollection(n).save(cs);})
-db.sites.update({'SITE_ID':site_id},{'$set' : {"CONSO" : db.getCollection(n).find().toArray()}});
